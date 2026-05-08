3cbfdafde321598a488f060cf241c72487190b5c
Ahmad Khidir <khidirahmad05@gmail.com>
2026-05-08 23:18:02 +0100

    feat: update Terraform backend configuration to use S3 for state management

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,11 @@ provider "aws" {}
 data "aws_region" "current" {}
 
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket       = "vanguard-op-terraform-state"
+    key          = "pantry-pilot/terraform.tfstate"
+    use_lockfile = true
+  }
 
   required_providers {
     aws = {
