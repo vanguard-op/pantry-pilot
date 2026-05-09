09ec5e4c50fb5855c8a239fa092a9595261eef0d
Ahmad Khidir <khidirahmad05@gmail.com>
2026-05-09 01:13:32 +0100

    feat: update database configuration in .env.example and fix callback URL in auth.tf

--- infra/auth.tf
+++ infra/auth.tf
@@ -18,7 +18,7 @@ resource "aws_cognito_user_pool" "this" {
 resource "aws_cognito_user_pool_client" "this" {
   name                                 = "pantry-pilot-client"
   user_pool_id                         = aws_cognito_user_pool.this.id
-  callback_urls                        = ["pantrypilot://auth", "http://localhost:8000/docs/oauth2-redirect", "https://1ilmkbnsk8.execute-api.us-east-1.amazonaws.com/docs/oauth2-redirect"]
+  callback_urls                        = ["pantrypilot://auth", "http://localhost:8000/docs/oauth2-redirect", "https://hcpmu33z78.execute-api.us-east-1.amazonaws.com/docs/oauth2-redirect"]
   logout_urls                          = ["pantrypilot://auth/logout"]
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code"]
